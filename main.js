3320e2355f69b70133a61e9608e35d47126dbe67
senselogic <dev.senselogic@gmail.com>
2023-10-24 22:51:32 +0200

    % event

--- main.js
+++ main.js
@@ -74,147 +74,151 @@ module.exports = class Atlas extends Plugin
 
     // ~~
 
+    updateTitle(
+        )
+    {
+        let oldBreadcrumbElement = document.getElementById( 'atlas-breadcrumb' );
+        let oldLinkListElement = document.getElementById( 'atlas-link-list' );
+
+        if ( oldBreadcrumbElement )
+        {
+            oldBreadcrumbElement.remove();
+        }
+
+        if ( oldLinkListElement )
+        {
+            oldLinkListElement.remove();
+        }
+
+        let titleElement = document.querySelector( '.inline-title' );
+        let mode = app.workspace.activeLeaf.getViewState().state.mode;
+
+        if ( titleElement
+             && mode === 'preview' )
+        {
+            let activeFile = this.app.workspace.getActiveFile();
+            let activeFilePath = activeFile.path;
+
+            if ( activeFilePath.endsWith( '.md' ) )
+            {
+                let superFolderArray = [];
+
+                if ( activeFile.parent )
+                {
+                    for ( let superFolder = activeFile.parent;
+                          superFolder;
+                          superFolder = superFolder.parent )
+                    {
+                        if ( superFolder.path !== '/' )
+                        {
+                            superFolderArray.push( superFolder );
+                        }
+                    }
+                }
+
+                let subFolderPath = activeFilePath.slice( 0, -3 );
+                let subFolder = this.app.vault.getAbstractFileByPath( subFolderPath );
+                let subFileArray = [];
+
+                if ( subFolder
+                     && subFolder instanceof TFolder )
+                {
+                    for ( let file of subFolder.children )
+                    {
+                        if ( file instanceof TFile
+                             && file.parent === subFolder
+                             && file.name.endsWith( '.md' ) )
+                        {
+                            subFileArray.push( file );
+                        }
+                    }
+                }
+
+                if ( superFolderArray.length >= 0 )
+                {
+                    let breadcrumbElement = document.createElement( 'div' );
+                    breadcrumbElement.setAttribute( 'id', 'atlas-breadcrumb' );
+                    breadcrumbElement.style.display = 'flex';
+                    breadcrumbElement.style.gap = '0.5rem';
+                    breadcrumbElement.style.fontSize = '0.7rem';
+                    breadcrumbElement.style.transform = 'translateY(-20px)';
+                    breadcrumbElement.style.position = 'absolute';
+
+                    for ( let superFolderIndex = superFolderArray.length - 1;
+                          superFolderIndex >= 0;
+                          --superFolderIndex )
+                    {
+                        let superFolder = superFolderArray[ superFolderIndex ];
+
+                        let linkElement = document.createElement( 'a' );
+                        linkElement.setAttribute( 'class', 'internal-link' );
+                        linkElement.setAttribute( 'data-href', superFolder.name );
+                        linkElement.setAttribute( 'data-tooltip-position', 'top' );
+                        linkElement.setAttribute( 'aria-label', superFolder.name );
+                        linkElement.setAttribute( 'href', superFolder.path );
+                        linkElement.setAttribute( 'rel', 'noopener' );
+                        linkElement.setAttribute( 'target', '_blank' );
+                        linkElement.textContent = superFolder.name;
+
+                        breadcrumbElement.appendChild( linkElement );
+
+                        if ( superFolderIndex > 0 )
+                        {
+                            let slashElement = document.createElement( 'span' );
+                            slashElement.textContent = '/';
+                            breadcrumbElement.appendChild( slashElement );
+                        }
+                    }
+
+                    titleElement.insertAdjacentElement( 'beforeBegin', breadcrumbElement );
+                }
+
+                if ( subFileArray.length >= 0 )
+                {
+                    let linkListElement = document.createElement( 'div' );
+                    linkListElement.setAttribute( 'id', 'atlas-link-list' );
+                    linkListElement.style.display = 'flex';
+                    linkListElement.style.flexDirection = 'column';
+                    linkListElement.style.gap = '0.5rem';
+                    linkListElement.style.fontSize = '0.7rem';
+
+                    for ( let subFile of subFileArray )
+                    {
+                        let linkElement = document.createElement( 'a' );
+                        linkElement.setAttribute( 'class', 'internal-link' );
+                        linkElement.setAttribute( 'data-href', subFile.name.slice( 0, -3 ) );
+                        linkElement.setAttribute( 'data-tooltip-position', 'top' );
+                        linkElement.setAttribute( 'aria-label', subFile.name.slice( 0, -3 ) );
+                        linkElement.setAttribute( 'href', subFile.path );
+                        linkElement.setAttribute( 'rel', 'noopener' );
+                        linkElement.setAttribute( 'target', '_blank' );
+                        linkElement.textContent = subFile.name;
+                        linkListElement.appendChild( linkElement );
+                    }
+
+                    titleElement.insertAdjacentElement( 'afterEnd', linkListElement );
+                }
+            }
+        }
+    }
+
+    // ~~
+
     async onload(
         )
     {
         console.log( 'Atlas plugin loaded' );
 
         await this.loadSettings();
-/*
-        this.registerEvent(
-            this.app.vault.on(
-                'renderPreviewFinished',
-                ( file, element ) =>
-                {
-                    console.log( 'render preview finished' );
 
-                    if ( file === this.app.workspace.getActiveFile() )
-                    {
-                        console.log( 'this is the active file' );
-                    }
+        this.registerEvent(
+            this.app.workspace.on(
+                'layout-change',
+                () =>
+                {
+                    this.updateTitle();
                 }
                 )
-            );
-*/
-        this.registerMarkdownPostProcessor(
-            ( element, elementData ) =>
-            {
-console.log( 'element', element );
-console.log( 'elementData', elementData );
-                let titleElement = element.querySelector( 'p' );
-console.log( 'titleElement', titleElement );
-                if ( titleElement )
-                {
-                    let activeFile = this.app.workspace.getActiveFile();
-console.log( 'activeFile', activeFile );
-                    let activeFilePath = activeFile.path;
-
-                    if ( activeFilePath.endsWith( '.md' ) )
-                    {
-                        let superFolderArray = [];
-
-                        if ( activeFile.parent )
-                        {
-                            for ( let superFolder = activeFile.parent;
-                                  superFolder;
-                                  superFolder = superFolder.parent )
-                            {
-                                if ( superFolder.path !== '/' )
-                                {
-                                    superFolderArray.push( superFolder );
-                                }
-                            }
-                        }
-console.log( 'superFolderArray', superFolderArray );
-
-                        let subFolderPath = '';
-
-                        if ( activeFilePath.indexOf( '/' ) >= 0 )
-                        {
-                            subFolderPath = activeFilePath.slice( 0, activeFilePath.lastIndexOf( '/' ) );
-                        }
-console.log( 'subFolderPath', subFolderPath );
-
-                        let subFolder = this.app.vault.getAbstractFileByPath( subFolderPath );
-
-                        if ( subFolder instanceof TFile )
-                        {
-                            subFolder = null;
-                        }
-console.log( 'subFolder', subFolder );
-                        let subFileArray = [];
-
-                        if ( subFolder )
-                        {
-                            for ( let file of subFolder.children )
-                            {
-                                if ( file instanceof TFile
-                                     && file.parent === subFolder
-                                     && file.name.endsWith( '.md' ) )
-                                {
-                                    subFileArray.push( file );
-                                }
-                            }
-                        }
-console.log( 'subFileArray', subFileArray );
-
-
-                        let breadcrumbElement = document.createElement( 'div' );
-                        breadcrumbElement.style.display = 'flex';
-                        breadcrumbElement.style.gap = '0.5rem';
-
-                        for ( let superFolderIndex = superFolderArray.length - 1;
-                              superFolderIndex >= 0;
-                              --superFolderIndex )
-                        {
-                            let superFolder = superFolderArray[ superFolderIndex ];
-
-                            let linkElement = document.createElement( 'a' );
-                            linkElement.setAttribute( 'data-tooltip-position', 'top' );
-                            linkElement.setAttribute( 'aria-label', superFolder.name );
-                            linkElement.setAttribute( 'data-href', superFolder.name );
-                            linkElement.setAttribute( 'href', superFolder.path );
-                            linkElement.setAttribute( 'class', 'internal-link' );
-                            linkElement.setAttribute( 'target', '_blank' );
-                            linkElement.setAttribute( 'rel', 'noopener' );
-                            linkElement.textContent = superFolder.name;
-
-                            breadcrumbElement.appendChild( linkElement );
-
-                            if ( superFolderIndex > 0 )
-                            {
-                                let slashElement = document.createElement( 'span' );
-                                slashElement.textContent = '/';
-                                breadcrumbElement.appendChild( slashElement );
-                            }
-                        }
-console.log( 'breadcrumbElement', breadcrumbElement );
-                        titleElement.insertAdjacentElement( 'beforeBegin', breadcrumbElement );
-
-                        let linkListElement = document.createElement( 'div' );
-                        linkListElement.style.display = 'flex';
-                        linkListElement.style.flexDirection = 'column';
-                        linkListElement.style.gap = '0.5rem';
-
-                        for ( let subFile of subFileArray )
-                        {
-                            let linkElement = document.createElement( 'a' );
-                            linkElement.setAttribute( 'data-tooltip-position', 'top' );
-                            linkElement.setAttribute( 'aria-label', subFile.name );
-                            linkElement.setAttribute( 'data-href', subFile.name );
-                            linkElement.setAttribute( 'href', subFile.path );
-                            linkElement.setAttribute( 'class', 'internal-link' );
-                            linkElement.setAttribute( 'target', '_blank' );
-                            linkElement.setAttribute( 'rel', 'noopener' );
-                            linkElement.textContent = subFile.name;
-                            linkListElement.appendChild( linkElement );
-                        }
-console.log( 'linkListElement', linkListElement );
-                        titleElement.insertAdjacentElement( 'afterEnd', linkListElement );
-console.log( 'titleElement', titleElement );
-                    }
-                }
-            }
             );
 
         this.addSettingTab(
